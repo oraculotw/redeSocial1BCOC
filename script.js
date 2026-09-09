8c5d003596c78905e5345bd0a7828e068e5f8644
oraculotw <107263071+oraculotw@users.noreply.github.com>
2026-09-09 11:18:52 -0300

    ok

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", ()=>{
     const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+    if (!likeBtn) return;
+
+    const likesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+    let isLiked = false;
+    let baseLikes = 0;
+    
+
+})
