500c833e959862b2c0cc368be14444c548c33fc3
oraculotw <107263071+oraculotw@users.noreply.github.com>
2026-09-09 11:47:48 -0300

    ok

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     let isLiked = false;
     let baseLikes = 0;
-    
+
+    //texto inicial
+
+    if(likesCountSpan){
+        likesCountSpan.textContent = "0";
+    }
+
+//numeros acima de 1mil
+
+function formatLikes (num){
+    if (num >=1000){
+        return (num/1000).toFixed(1)+"K";
+    }
+    return num.toString();
+}
+
+
 
 })
